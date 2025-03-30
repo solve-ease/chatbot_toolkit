0fb2c12f7831c048d3437808e2b6140c932b375a
Anmo!Goe! <88969348+Anmolgoel29@users.noreply.github.com>
2025-03-30 15:51:12 +0530

    llm_application v1

---
+++ llm_application/llm_app.py
@@ -0,0 +1,30 @@
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class llm:
+    def __init__(self, model_name : str , model_provider : str):
+        self.model = init_chat_model(
+            model=model_name,
+            model_provider=model_provider
+        )
+
+    def create_prompt_template(self , template: ChatPromptTemplate): # pass a prompt template to maintain while calling the llm
+        self.prompt_template = template
+
+    def stream(self, inp: tuple):
+        for i in self.model.stream(
+            self.prompt_template.invoke(
+                inp
+            )
+        ):
+            yield i
+        
+    def invoke(self, inp : tuple):
+        return self.model.invoke(
+            self.prompt_template.invoke(
+                inp
+            )
+        )
